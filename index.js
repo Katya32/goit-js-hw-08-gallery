63a09a6ce176cd5ab23663b07fb45577e3988154
Katya Kondratchuk <68007177+Katya32@users.noreply.github.com>
2021-07-23 22:58:19 +0300

    hw-8

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import {galleryItems} from "./app.js";
 
-const ulListRef = document.querySelector(".js-gallery");
-const modalRef = document.querySelector(".js-lightbox");
-const modalImageRef = document.querySelector(".lightbox__image");
+const ulListRef = document.querySelector(".js-gallery"); //ссылка куда вставлять разметку
+const modalRef = document.querySelector(".js-lightbox"); //ссылка на модальное окно
+const modalImageRef = document.querySelector(".lightbox__image");//ссылка на img, который будет показывать полноразмерное изображения
 
-function createImageMarkup(galleryItems) {
+function createImageMarkup(galleryItems) { //функция перебирает массив с картинками и возвращает разметку
     return galleryItems.map(({preview, original, description}) => {
         return `<li class="gallery__item">
         <a
@@ -16,60 +16,61 @@ function createImageMarkup(galleryItems) {
         src="${preview}"
         data-source="${original}"
         alt="${description}"
-      />
-      </a>
-  </li>
-  `}).join('')
-};
+        />
+        </a>
+        </li>
+        `}).join('')
+    };
+    
+ulListRef.insertAdjacentHTML("beforeend", createImageMarkup(galleryItems)); //вставляем разметку
 
-ulListRef.insertAdjacentHTML("beforeend", createImageMarkup(galleryItems));
 
-const galleryImageRef = document.querySelectorAll(".gallery__image");
-const currentIndex = [...galleryImageRef].map((item, index) => item.dataset.index = `${index}`);
-
-const modalIsOpen = (e)=>{
-    if(!e.target.classList.contains("gallery__image")) {
+const modalIsOpen = (e)=>{ //функция открытия модального окна
+    if(!e.target.classList.contains("gallery__image")) { //проверка на клик именно на img. если не img, то выходим
         return;
     }
-    e.preventDefault();
-    modalRef.classList.add("is-open");
+    e.preventDefault(); //отмена действий браузера по умолчанию
+    modalRef.classList.add("is-open"); //при клике добавляем класс is-open и в img записываем атрибуты
     modalImageRef.src = e.target.dataset.source;
     modalImageRef.alt = e.target.alt;
     modalImageRef.dataset.index = e.target.dataset.index;
 };
 
-const modalIsClose = (e) =>{
-    if(e.target.classList.contains("lightbox__button") || e.target.classList.contains("lightbox__overlay")) {
+const modalIsClose = (e) =>{ //функция закрытия модального окна
+    if(e.target.classList.contains("lightbox__button") || e.target.classList.contains("lightbox__overlay")) {//если нажали на button или на overlay
+        modalRef.classList.remove("is-open");// снимаем класс is-open и в img обнуляем атрибуты
+        modalImageRef.src = "";
+        modalImageRef.alt = "";
+        modalImageRef.dataset.index = "";
+    }
+    return;
+}
+
+
+const galleryImageRef = document.querySelectorAll(".gallery__image"); //ссылка на все img 
+galleryImageRef.forEach((item, index) => item.dataset.index = `${index}`); //в каждый img добавляем атрибут data-index значением которого будет индекс
+
+const closeModalEscAndSlider = (e) => { //при нажатии на клавишу Esc атрибуты обнуляются и модальное окно закрывается
+    if(e.key === "Escape") {
         modalRef.classList.remove("is-open");
         modalImageRef.src = "";
         modalImageRef.alt = "";
         modalImageRef.dataset.index = "";
-}
-return;
-}
-
-
-const closeModalEsc = (e) => {
-    if(e.key === "Escape") {
-    modalRef.classList.remove("is-open");
-    modalImageRef.src = "";
-    modalImageRef.alt = "";
-    modalImageRef.dataset.index = "";
-  };
+    };
   
-  let index = Number(modalImageRef.dataset.index)
+    let currentIndexImageInModal = Number(modalImageRef.dataset.index)//в переменную currentIndexImageInModal записываем индекс текущей картинки в модальном окне
 
-  if(e.key === "ArrowRight" && index <= galleryItems.length) { 
-    modalImageRef.src = galleryItems[index+1].original;
-    modalImageRef.dataset.index = index +1
-  }
-  if(e.key === "ArrowLeft"&& index >= 0) {
-    modalImageRef.src = galleryItems[index-1].original;
-    modalImageRef.dataset.index = index -1
+    if(e.key === "ArrowRight" && currentIndexImageInModal <= galleryItems.length) { //если нажимаем ArrowRight и индекс <= длины массива
+        modalImageRef.src = galleryItems[currentIndexImageInModal + 1 ].original; // в src текищей картинки перезаписывается src следующей картинки
+        modalImageRef.dataset.index = currentIndexImageInModal + 1;// а в data-index текущий индекс +1
+    } 
+    if(e.key === "ArrowLeft" && currentIndexImageInModal >= 0) {
+        modalImageRef.src = galleryItems[currentIndexImageInModal-1].original;
+        modalImageRef.dataset.index = currentIndexImageInModal -1;
     }
 };
 
 
 ulListRef.addEventListener("click", modalIsOpen);
 modalRef.addEventListener("click", modalIsClose);
-window.addEventListener("keydown", closeModalEsc);
+window.addEventListener("keydown", closeModalEscAndSlider);
